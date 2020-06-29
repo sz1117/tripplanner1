5d8f65371bd107d161a74de6d743f1c48daf480d
Simon Zeng <zeng.simonl@gmail.com>
2020-06-29 11:46:00 -0400

    Added API

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-console.log("Hello from Javascript");
+// console.log("Hello from Javascript");
+
+const mapboxgl = require('mapbox-gl');
+
+mapboxgl.accessToken = 'pk.eyJ1Ijoic3oxMTE3IiwiYSI6ImNrYzBucHp2ZTEzMmcyc3BqaGM2djMxbDYifQ.XHcH-avolUHVF8udNCYhYA';
+
+const map = new mapboxgl.Map({
+	container : 'map',
+	center    : [ -74.009, 40.705 ], // FullStack NY coordinates; alternatively, use [-87.6354, 41.8885] for Chicago
+	zoom      : 12, // starting zoom
+	style     : 'mapbox://styles/mapbox/streets-v10' // mapbox has lots of different map styles available.
+});
